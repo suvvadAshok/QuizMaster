35e17ffdaf206eeb5294f526a6a4eaa7dd42160c
ashokadvait <ashok@advaitlabs.com>
2024-09-22 07:05:51 +0530

    add redirects value

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig(({ mode }) => {
-  console.log("Vite Mode:", mode); // Check if mode is correctly detected
+  console.log("Vite Mode:", mode);
   return {
     plugins: [react()],
-    base: mode === "production" ? "/Client" : "/Client/",
+    base: "/Client",
     build: {
       outDir: "dist",
       assetsDir: "assets",
