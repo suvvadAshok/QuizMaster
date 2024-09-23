4376a44b276f6b417fb0b2630a96a5e66f5883c7
ashokadvait <ashok@advaitlabs.com>
2024-09-23 16:39:03 +0530

    remove client related files

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({ mode }) => {
-  // Load environment variables based on the mode (development or production)
-  const env = loadEnv(mode, process.cwd());
-  
-  const basePath = env.VITE_BASE_PATH || "/Client/dist/";
-
-  console.log("Base Path:", basePath); // Check what base path is being set
-
+export default defineConfig(() => {
   return {
     plugins: [react()],
-    base: basePath, 
-    publicDir: "public",
     build: {
       outDir: "dist",
       assetsDir: "assets",
