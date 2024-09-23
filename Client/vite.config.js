cd770d171b11d0767bc95d6a399b6c31b2269e11
ashokadvait <ashok@advaitlabs.com>
2024-09-23 15:34:24 +0530

    add base path

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,8 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig(({ mode }) => {
   // Load environment variables based on the mode (development or production)
   const env = loadEnv(mode, process.cwd());
-
-  // Use the VITE_BASE_PATH or fallback to "/Client/"
+  
   const basePath = env.VITE_BASE_PATH || "/Client/dist/";
 
   console.log("Base Path:", basePath); // Check what base path is being set
