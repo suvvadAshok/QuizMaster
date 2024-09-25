d6c2c88c2181efcd3b38e240a63f22ff21d93edb
ashokadvait <ashok@advaitlabs.com>
2024-09-25 18:41:27 +0530

    add animation to path

--- src/components/questionSteps.jsx
+++ src/components/questionSteps.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 import { OnTopCurve } from "../assets/images/ontopCurve";
-import one from "../assets/images/number_1.svg";
-import two from "../assets/images/number_2.svg";
-import three from "../assets/images/number_3.svg";
-import four from "../assets/images/number_4.svg";
-import five from "../assets/images/number_5.svg";
+import { QuestionOne } from "../assets/images/number_1.jsx";
+import { QuestionTwo } from "../assets/images/number_2.jsx";
+import { QuestionThree } from "../assets/images/number_3.jsx";
+import { QuestionFour } from "../assets/images/number_4.jsx";
+import { QuestionFive } from "../assets/images/number_5.jsx";
 import React from "react";
 
 QuestionSteps.propTypes = {
@@ -25,17 +25,16 @@ export function QuestionSteps({ setQNum, qNum, answers }) {
   return (
     <>
       <div className="relative ">
-        <OnTopCurve />
+        <OnTopCurve qNum={qNum} />
         <div>
           <button
             disabled={able[0] === "notAble"}
             onClick={() => setQNum(0)}
             className="absolute top-[1%] left-[2%]"
           >
-            <img
-              src={one}
-              alt=""
-              // className={`${able[0] === "able" && "bg-green-400"}`}
+            <QuestionOne
+              fill={able[0] === "notAble" ? "#fff" : "#FBC02D"}
+              stroke={able[0] === "notAble" ? "#FBC02D" : "#fff"}
             />
           </button>
           <button
@@ -43,28 +42,40 @@ export function QuestionSteps({ setQNum, qNum, answers }) {
             onClick={() => setQNum(1)}
             className="absolute top-[20%] left-[25%] "
           >
-            <img src={two} alt="" />
+            <QuestionTwo
+              fill={able[1] === "notAble" ? "#fff" : "#FBC02D"}
+              stroke={able[1] === "notAble" ? "#FBC02D" : "#fff"}
+            />
           </button>
           <button
             disabled={able[2] === "notAble"}
             onClick={() => setQNum(2)}
-            className="absolute top-[40%] left-[50%] "
+            className="absolute top-[40%] left-[54%] "
           >
-            <img src={three} alt="" />
+            <QuestionThree
+              fill={able[2] === "notAble" ? "#fff" : "#FBC02D"}
+              stroke={able[2] === "notAble" ? "#FBC02D" : "#fff"}
+            />
           </button>
           <button
             disabled={able[3] === "notAble"}
             onClick={() => setQNum(3)}
-            className="absolute bottom-[70%] right-[25%] "
+            className="absolute bottom-[76%] right-[25%] "
           >
-            <img src={four} alt="" />
+            <QuestionFour
+              fill={able[2] === "notAble" ? "#fff" : "#FBC02D"}
+              stroke={able[2] === "notAble" ? "#FBC02D" : "#fff"}
+            />
           </button>
           <button
             disabled={able[4] === "notAble"}
             onClick={() => setQNum(4)}
-            className="absolute top-[16%] right-[8%] "
+            className="absolute top-[40%] right-[8%] "
           >
-            <img src={five} alt="" />
+            <QuestionFive
+              fill={able[2] === "notAble" ? "#fff" : "#FBC02D"}
+              stroke={able[2] === "notAble" ? "#FBC02D" : "#fff"}
+            />
           </button>
         </div>
       </div>
